7812f06b30ea130ef3e78e349c681e01fe55e4d7
tayomadein <tuby1805@gmail.com>
2018-07-11 15:40:10 +0100

    show multiple map locations

---
+++ js/data.js
@@ -0,0 +1,58 @@
+var mapLocations = [
+    {
+        title: "Taj Restaurant",
+        location: {
+            lat: 6.553472,
+            lng: 3.365529,
+        }
+    },
+    {
+        title: "Kasa Indiana Restaurant",
+        location: {
+            lat: 6.557661,
+            lng: 3.361177,
+        }
+    },
+    {
+        title: "Oasis Restaurant",
+        location: {
+            lat: 6.552214,
+            lng: 3.361445,
+        }
+    },
+    {
+        title: "SPAR Ilupeju",
+        location: {
+            lat: 6.556567,
+            lng: 3.359885,
+        }
+    },
+    {
+        title: "Chicken Republic",
+        location: {
+            lat: 6.546845,
+            lng: 3.362431,
+        }
+    },
+    {
+        title: "Foodcourt - Maryland Mall",
+        location: {
+            lat: 6.567284,
+            lng: 3.367245,
+        }
+    },
+    {
+        title: "Domino's Pizza Ilupeju",
+        location: {
+            lat: 6.547996,
+            lng: 3.361654,
+        }
+    },
+    {
+        title: "Gypsy’s Restaurant & Bar",
+        location: {
+            lat: 6.548254,
+            lng: 3.364976
+        }
+    }
+];
--- js/index.js
+++ js/index.js
@@ -4,16 +4,26 @@ $(document).ready(function () {
 });
 
 /* Map */
-var map;
+var map, marker;
 // Initialize and add the map
 function initMap() {
     var andela = { lat: 6.553818, lng: 3.366543 };
     map = new google.maps.Map(
         document.getElementById('map'), { zoom: 15, center: andela });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: andela, map: map });
+    // The marker, positioned at Andela
+    marker = new google.maps.Marker({ position: andela, map: map });
+    showLocations(mapLocations);
 }
 
 function mapError() {
     alert('An error occurred while loading the maps');
+}
+
+//var lunchLocations = ko.applyBindings({ mapLocations })
+//console.log(lunchLocations);
+
+function showLocations(mapLocations  ) {
+    for (var i = 0; i < mapLocations.length; i++) {
+        marker = new google.maps.Marker({ position: mapLocations[i].location, map });
+    }
 }
